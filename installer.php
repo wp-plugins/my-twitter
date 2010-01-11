94c7e26251e54eecc79156624fdfc48d58ac0ba6
xhanch <xhanch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-11 17:48:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/my-twitter/trunk@192781 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ installer.php
@@ -0,0 +1,17 @@
+<?php
+	require_once(ABSPATH . 'wp-admin/upgrade.php');
+
+	global $wpdb;
+	$my_twitter_version = "1.0";
+
+	$cur_ver = get_option( "my_twitter_version" );
+
+	if($cur_ver === false){
+		add_option("my_twitter_title", "Latest Tweets");
+		add_option("my_twitter_id", "");
+		add_option("my_twitter_count", "5");
+
+		add_option("my_twitter_version", $my_twitter_version);
+	}else if( $cur_ver != $my_twitter_version)
+		update_option( "my_twitter_version", $my_twitter_version );
+?>
